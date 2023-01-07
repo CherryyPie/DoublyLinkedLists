ce567a3a9b6b40a4c9639a5c388bf3970f57ce38
kiu_prozorovi.elene <prozorovi.elene@kiu.edu.ge>
2023-01-08 02:31:09 +0400

    no message

--- src/fop/w5dll/IntDoubleList.java
+++ src/fop/w5dll/IntDoubleList.java
@@ -1,5 +1,38 @@
 package fop.w5dll;
 
 public class IntDoubleList {
-	// TODO
+    private IntDoubleListElement head;
+    private IntDoubleListElement tail;
+
+    public IntDoubleList() {
+        head = null;
+        tail = null;
+    }
+
+    public IntDoubleListElement getFirstElement() {
+        return head;
+    }
+
+    public IntDoubleListElement getLastElement() {
+        return tail;
+    }
+
+    public void append(int info) {
+        IntDoubleListElement elem = new IntDoubleListElement(info);
+        if (head == null && tail == null) {
+            head = elem;
+            tail = elem;
+        } else {
+            tail.next = elem;
+            elem.prev = tail;
+            elem = tail;
+        }
+    }
+	public int size(){
+        int count = 0;
+        while (head != null){
+            head = head.next;
+            count++;
+        }return count;
+    }
 }
--- src/fop/w5dll/IntDoubleListElement.java
+++ src/fop/w5dll/IntDoubleListElement.java
@@ -1,5 +1,17 @@
 package fop.w5dll;
 
 public class IntDoubleListElement {
-	// TODO
+	private int info;
+    public IntDoubleListElement next;
+    public IntDoubleListElement prev;
+
+    public IntDoubleListElement(int info) {
+        this.info = info;
+        next = null;
+        prev = null;
+    }
+
+    public int getInfo() {
+        return info;
+    }
 }
